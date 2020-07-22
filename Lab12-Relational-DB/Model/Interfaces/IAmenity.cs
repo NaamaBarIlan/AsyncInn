180bf28bcad310da20ffa18b7a297d35f9c7a161
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-22 14:39:57 -0700

    Added the interface IAmenity

---
+++ Lab12-Relational-DB/Model/Interfaces/IAmenity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12_Relational_DB.Model.Interfaces
+{
+    public interface IAmenity
+    {
+        // Create
+        Task<Amenity> Create(Amenity amenity);
+
+        // Read
+        // Get ALL
+        Task<List<Amenity>> GetAmenities();
+
+        // Get individually (by id)
+        Task<Amenity> GetAmenity(int id);
+
+        // Update
+        Task<Amenity> Update(Amenity amenity);
+
+        // Delete
+        Task Delete(int id);
+    }
+}
