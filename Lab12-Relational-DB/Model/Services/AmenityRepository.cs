1f15b844e0e60dd23295dbdacb0a2b840d70da15
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-22 15:02:13 -0700

    Created the service AmenityRepository Updated the AmenitiesController

---
+++ Lab12-Relational-DB/Model/Services/AmenityRepository.cs
@@ -0,0 +1,57 @@
+﻿using Lab12_Relational_DB.Data;
+using Lab12_Relational_DB.Model.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12_Relational_DB.Model.Services
+{
+    public class AmenityRepository : IAmenity
+    {
+        private AsyncInnDbContext _context;
+
+        public AmenityRepository(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Amenity> Create(Amenity amenity)
+        {
+            _context.Entry(amenity).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            await _context.SaveChangesAsync();
+
+            return amenity;
+        }
+
+        public async Task Delete(int id)
+        {
+            Amenity amenity = await GetAmenity(id);
+
+            _context.Entry(amenity).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Amenity>> GetAmenities()
+        {
+            var amenities = await _context.Amenities.ToListAsync();
+            return amenities;
+        }
+
+        public async Task<Amenity> GetAmenity(int id)
+        {
+            Amenity amenity = await _context.Amenities.FindAsync(id);
+            return amenity;
+        }
+
+        public async Task<Amenity> Update(Amenity amenity)
+        {
+            _context.Entry(amenity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return amenity;
+        }
+    }
+}
