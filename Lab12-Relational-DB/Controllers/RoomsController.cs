ee489b235a132f44be00bcd0443801a505327be0
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-22 14:03:22 -0700

    Added IRoom interface and RoomsController

--- Lab12-Relational-DB/Controllers/RoomsController.cs
+++ Lab12-Relational-DB/Controllers/RoomsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Lab12_Relational_DB.Data;
 using Lab12_Relational_DB.Model;
+using Lab12_Relational_DB.Model.Interfaces;
 
 namespace Lab12_Relational_DB.Controllers
 {
@@ -14,31 +15,25 @@ namespace Lab12_Relational_DB.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
-        private readonly AsyncInnDbContext _context;
+        private readonly IRoom _room;
 
-        public RoomsController(AsyncInnDbContext context)
+        public RoomsController(IRoom room)
         {
-            _context = context;
+            _room = room;
         }
 
         // GET: api/Rooms
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Room>>> GetRooms()
         {
-            return await _context.Rooms.ToListAsync();
+            return await _room.GetRooms();
         }
 
         // GET: api/Rooms/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Room>> GetRoom(int id)
         {
-            var room = await _context.Rooms.FindAsync(id);
-
-            if (room == null)
-            {
-                return NotFound();
-            }
-
+            Room room = await _room.GetRoom(id);
             return room;
         }
 
@@ -53,25 +48,8 @@ namespace Lab12_Relational_DB.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(room).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RoomExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            var updatedRoom = await _room.Update(room);
+            return Ok(updatedRoom);
         }
 
         // POST: api/Rooms
@@ -80,8 +58,7 @@ namespace Lab12_Relational_DB.Controllers
         [HttpPost]
         public async Task<ActionResult<Room>> PostRoom(Room room)
         {
-            _context.Rooms.Add(room);
-            await _context.SaveChangesAsync();
+            await _room.Create(room);
 
             return CreatedAtAction("GetRoom", new { id = room.Id }, room);
         }
@@ -90,21 +67,9 @@ namespace Lab12_Relational_DB.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<Room>> DeleteRoom(int id)
         {
-            var room = await _context.Rooms.FindAsync(id);
-            if (room == null)
-            {
-                return NotFound();
-            }
-
-            _context.Rooms.Remove(room);
-            await _context.SaveChangesAsync();
-
-            return room;
+            await _room.Delete(id);
+            return NoContent();
         }
 
-        private bool RoomExists(int id)
-        {
-            return _context.Rooms.Any(e => e.Id == id);
-        }
     }
 }
---
+++ Lab12-Relational-DB/Model/Interfaces/IRoom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12_Relational_DB.Model.Interfaces
+{
+    public interface IRoom
+    {
+        // Create
+        Task<Room> Create(Room room);
+
+        // Read
+        // Get All
+        Task<List<Room>> GetRooms();
+
+        // Get individually (by Id)
+        Task<Room> GetRoom(int id);
+
+        // Update
+        Task<Room> Update(Room room);
+
+        // Delete
+        Task Delete(int id);
+    }
+}
