b24db8c33429ba565b35bd23d05b329ca49eb953
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-27 16:24:12 -0700

    Updated HotelController.cs

---
+++ Lab12-Relational-DB/Controllers/HotelRoomsController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lab12_Relational_DB.Data;
+using Lab12_Relational_DB.Model;
+using Lab12_Relational_DB.Model.Interfaces;
+
+namespace Lab12_Relational_DB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelRoomsController : ControllerBase
+    {
+        private readonly IHotelRoom _hotelRoom;
+
+        public HotelRoomsController(IHotelRoom hotelRoom)
+        {
+            _hotelRoom = hotelRoom;
+        }
+
+        // GET: api/Hotel/4/HotelRooms
+        [HttpGet, Route("/api/Hotels/{hotelId}/Rooms")]
+        public async Task<ActionResult<IEnumerable<HotelRoom>>> GetHotelRooms(int hotelId)
+        {
+            return await _hotelRoom.GetHotelRooms(hotelId);
+        }
+
+
+        // GET: api/HotelRooms/5
+        [HttpGet("/api/Hotels/{hotelId}/Rooms/{roomNumber}")]
+        public async Task<ActionResult<HotelRoom>> GetSingleHotelRoom(int roomNumber, int hotelId)
+        {
+            var hotelRoom = await _hotelRoom.GetSingleHotelRoom(roomNumber, hotelId);
+
+            if (hotelRoom == null)
+            {
+                return NotFound();
+            }
+
+            return hotelRoom;
+        }
+
+        // PUT: api/HotelRooms/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("/api/Hotels/{hotelId}/Rooms/{roomNumber}")]
+        public async Task<IActionResult> PutHotelRoom(int hotelId, int roomNumber, HotelRoom hotelRoom)
+        {
+            if (hotelId != hotelRoom.HotelId || roomNumber != hotelRoom.RoomNumber)
+            {
+                return BadRequest();
+            }
+
+            //update
+            await _hotelRoom.Update(hotelRoom);
+
+            return NoContent();
+        }
+
+        // POST: api/HotelRooms
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost, Route("/api/Hotels/{hotelId}/Rooms")]
+        public async Task<ActionResult<HotelRoom>> PostHotelRoom(HotelRoom hotelRoom, int hotelId)
+        {
+            await _hotelRoom.Create(hotelRoom, hotelId);
+
+            return CreatedAtAction("GetHotelRoom", new { id = hotelRoom.HotelId }, hotelRoom);
+        }
+
+        // DELETE: api/HotelRooms/5
+        [HttpDelete("/api/Hotels/{hotelId}/Rooms/{roomNumber}")]
+        public async Task<ActionResult<HotelRoom>> DeleteHotelRoom(int roomNumber, int hotelId)
+        {
+            await _hotelRoom.Delete(roomNumber, hotelId);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Lab12-Relational-DB/Migrations/20200727205710_addingHotelRoom.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab12_Relational_DB.Migrations
+{
+    public partial class addingHotelRoom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HotelRoom_RoomId",
+                table: "HotelRoom");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenities_AmenityId",
+                table: "RoomAmenities",
+                column: "AmenityId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelRoom_RoomId",
+                table: "HotelRoom",
+                column: "RoomId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RoomAmenities_AmenityId",
+                table: "RoomAmenities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HotelRoom_RoomId",
+                table: "HotelRoom");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelRoom_RoomId",
+                table: "HotelRoom",
+                column: "RoomId");
+        }
+    }
+}
