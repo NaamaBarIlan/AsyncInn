bd5cb522f8e71c5b30f40a4cc13d74d6ff7cf9fa
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-27 13:33:10 -0700

    Updated IHotelRoom.cs

--- Lab12-Relational-DB/Model/Interfaces/IHotelRoom.cs
+++ Lab12-Relational-DB/Model/Interfaces/IHotelRoom.cs
@@ -8,13 +8,13 @@ namespace Lab12_Relational_DB.Model.Interfaces
 {
     public interface IHotelRoom
     {
-        Task<HotelRoom> Create(HotelRoom hotel);
+        Task<HotelRoom> Create(HotelRoom hotelroom, int hotelId);
 
-        Task Update(HotelRoom hotel);
+        Task Update(HotelRoom hotelroom);
 
         Task Delete(int roomNumber, int hotelId);
 
-        Task<List<HotelRoom>> GetRooms(int hotelId);
+        Task<List<HotelRoom>> GetHotelRooms(int hotelId);
 
         Task<HotelRoom> GetSingleHotelRoom(int roomNumber, int hotelId);
 
