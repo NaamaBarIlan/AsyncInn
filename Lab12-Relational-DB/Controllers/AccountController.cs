121379860ce43e0fba10fb83157c50803f52d1c7
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-28 20:09:07 -0700

    Added summary notes to AccountController

--- Lab12-Relational-DB/Controllers/AccountController.cs
+++ Lab12-Relational-DB/Controllers/AccountController.cs
@@ -11,6 +11,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Lab12_Relational_DB.Controllers
 {
+    /// <summary>
+    /// This API controller handles User Identity, 
+    /// including user registration and user login
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
@@ -24,6 +28,12 @@ namespace Lab12_Relational_DB.Controllers
             _signInManager = signInManager;
         }
 
+        /// <summary>
+        /// This custom API route handles user registration to the application
+        /// while implementing a RegisterDTO
+        /// </summary>
+        /// <param name="register">A unique RegisterDTO object</param>
+        /// <returns>Task result: either an Ok or a BadRequest message</returns>
         // api/account/register
         [HttpPost, Route("register")]
         public async Task<IActionResult> Register(RegisterDTO register)
@@ -49,16 +59,20 @@ namespace Lab12_Relational_DB.Controllers
 
             return BadRequest("Invalid Registration");
 
-            // do something to put this into the db
         }
 
+        /// <summary>
+        /// This custom API route handles user login to the application
+        /// while implementing a LoginDTO
+        /// </summary>
+        /// <param name="login">A unique LoginDTO object</param>
+        /// <returns>Task result: either an Ok or a BadRequest message</returns>
         [HttpPost, Route("Login")]
         public async Task<IActionResult> Login(LoginDTO login)
         {
             var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, false, false);
              if(result.Succeeded)
             {
-                // log the user in
                 return Ok("logged in");
             }
 
